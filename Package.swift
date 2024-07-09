db9fe74b9dd1e4c3613d24ad3a6e2aab62169355
Nathan Walker <walkerrunpdx@gmail.com>
2024-07-09 15:39:37 -0700

    chore: cleanup

--- Package.swift
+++ Package.swift
@@ -1,15 +1,34 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+// Copyright OpenJS Foundation and other contributors, https://openjsf.org
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 import PackageDescription
 
 let package = Package(
     name: "NativeScriptSDK",
     platforms: [
-        .iOS(.v13)  // or whichever minimum version you target
+        .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NativeScriptSDK",
             targets: ["NativeScript"]),
@@ -19,15 +38,6 @@ let package = Package(
         .binaryTarget(
             name: "NativeScript",
             path: "NativeScript.xcframework"
-        ),
-        // .target(
-        //     name: "NativeScriptSource", // This is your new source target
-        //     dependencies: ["NativeScript"], // It depends on the binary target
-        //     path: "Sources/NativeScriptSource", // This assumes you've added your Obj-C files in this directory
-        //     publicHeadersPath: ".", // Expose all headers in the directory as public
-        //     cSettings: [
-        //         .headerSearchPath(".") // Specify the path to your Objective-C files
-        //     ]
-        // )
+        )
     ]
 )
